5f30dbb2dee36859683fb394bfd8a917c0357a89
tlittrell <littrell.thomas@gmail.com>
2019-03-12 21:54:10 -0400

    Added basic contracts

--- src/k_median_optimization.py
+++ src/k_median_optimization.py
@@ -1,9 +1,9 @@
 from collections import namedtuple
 
 import networkx as nx
+from dpcontracts import require
 from pyomo.environ import *
 from pyomo.opt import SolverFactory
-from dpcontracts import require, types
 
 
 @require("max_facilities must be positive", lambda args: args.max_facilities > 0)
@@ -17,7 +17,9 @@ def optimize_station_placement(game_graph, max_facilities, require_atlanta=True)
 
     # each city must be assigned to one and only one research facility
     def city_assigned_to_research_station(model, city):
-        return sum(model.x[possible_facility, city] for possible_facility in cities) == 1
+        return (
+                sum(model.x[possible_facility, city] for possible_facility in cities) == 1
+        )
 
     m.each_city_assigned = Constraint(cities, rule=city_assigned_to_research_station)
 
@@ -28,21 +30,29 @@ def optimize_station_placement(game_graph, max_facilities, require_atlanta=True)
     m.active_facilities_only = Constraint(cities, cities, rule=assign_only_if_active)
 
     # limit the number of facilities
-    m.max_facilities = Constraint(expr=sum(m.y[possible_facility] for possible_facility in cities) <= max_facilities)
+    m.max_facilities = Constraint(
+        expr=sum(m.y[possible_facility] for possible_facility in cities)
+             <= max_facilities
+    )
 
     # Atlanta is the default station
     if require_atlanta:
-        m.put_station_in_atlanta = Constraint(expr=m.y['Atlanta'] == 1)
+        m.put_station_in_atlanta = Constraint(expr=m.y["Atlanta"] == 1)
 
     # objective function
-    m.obj = Objective(sense=minimize, expr=sum(
-        nx.shortest_path_length(game_graph, source=possible_facility, target=city) * m.x[possible_facility, city] for
-        possible_facility in cities for city in cities))
+    m.obj = Objective(
+        sense=minimize,
+        expr=sum(
+            nx.shortest_path_length(game_graph, source=possible_facility, target=city)
+            * m.x[possible_facility, city]
+            for possible_facility in cities
+            for city in cities
+        ),
+    )
 
-    solver = SolverFactory('cbc')
+    solver = SolverFactory("cbc")
     solver.solve(m, tee=False)
 
-    Output = namedtuple('station_placement', 'selections objective_value')
-    result = Output(tuple(city for city in cities if m.y[city] == 1),
-                    m.obj())
+    Output = namedtuple("station_placement", "selections objective_value")
+    result = Output(tuple(city for city in cities if m.y[city] == 1), m.obj())
     return result
--- src/test_pandemic_graph.py
+++ src/test_pandemic_graph.py
@@ -4,58 +4,57 @@ from src.game_graph import edges
 
 
 class TestGraph(object):
-
     def setup_method(self):
         self.g = nx.Graph()
         for i, cities in enumerate(edges):
             self.g.add_edge(cities[0], cities[1])
 
     def test_graph_degrees(self):
-        assert self.g.degree('Lima') == 4
-        assert self.g.degree('Mexico City') == 5
-        assert self.g.degree('Manila') == 5
-        assert self.g.degree('Taipei') == 4
-        assert self.g.degree('Osaka') == 2
-        assert self.g.degree('Tokyo') == 4
-        assert self.g.degree('Seoul') == 3
-        assert self.g.degree('Ho Chi Minh City') == 4
-        assert self.g.degree('Hong Kong') == 6
-        assert self.g.degree('Shanghai') == 5
-        assert self.g.degree('Beijing') == 2
-        assert self.g.degree('Sydney') == 3
-        assert self.g.degree('Jakarta') == 4
-        assert self.g.degree('Bangkok') == 4
-        assert self.g.degree('Mumbai') == 3
-        assert self.g.degree('Chennai') == 4
-        assert self.g.degree('Kolkata') == 4
-        assert self.g.degree('Delhi') == 5
-        assert self.g.degree('Karachi') == 4
-        assert self.g.degree('Tehran') == 4
-        assert self.g.degree('Riyadh') == 3
-        assert self.g.degree('Baghdad') == 4
-        assert self.g.degree('Moscow') == 3
-        assert self.g.degree('Lagos') == 3
-        assert self.g.degree('Kinshasa') == 3
-        assert self.g.degree('Khartoum') == 4
-        assert self.g.degree('Johannesburg') == 3
-        assert self.g.degree('Cairo') == 5
-        assert self.g.degree('Istanbul') == 6
-        assert self.g.degree('Algiers') == 4
-        assert self.g.degree('St Petersburg') == 3
-        assert self.g.degree('Milan') == 3
-        assert self.g.degree('Essen') == 4
-        assert self.g.degree('Atlanta') == 3
-        assert self.g.degree('Chicago') == 5
-        assert self.g.degree('Montreal') == 3
-        assert self.g.degree('Washington') == 4
-        assert self.g.degree('Los Angeles') == 5
-        assert self.g.degree('San Francisco') == 4
-        assert self.g.degree('Miami') == 4
-        assert self.g.degree('Bogota') == 5
-        assert self.g.degree('Santiago') == 2
-        assert self.g.degree('Sao Paulo') == 4
-        assert self.g.degree('Buenos Aires') == 4
-        assert self.g.degree('New York') == 4
-        assert self.g.degree('London') == 4
-        assert self.g.degree('Madrid') == 5
-        assert self.g.degree('Paris') == 5
+        assert self.g.degree("Lima") == 4
+        assert self.g.degree("Mexico City") == 5
+        assert self.g.degree("Manila") == 5
+        assert self.g.degree("Taipei") == 4
+        assert self.g.degree("Osaka") == 2
+        assert self.g.degree("Tokyo") == 4
+        assert self.g.degree("Seoul") == 3
+        assert self.g.degree("Ho Chi Minh City") == 4
+        assert self.g.degree("Hong Kong") == 6
+        assert self.g.degree("Shanghai") == 5
+        assert self.g.degree("Beijing") == 2
+        assert self.g.degree("Sydney") == 3
+        assert self.g.degree("Jakarta") == 4
+        assert self.g.degree("Bangkok") == 4
+        assert self.g.degree("Mumbai") == 3
+        assert self.g.degree("Chennai") == 4
+        assert self.g.degree("Kolkata") == 4
+        assert self.g.degree("Delhi") == 5
+        assert self.g.degree("Karachi") == 4
+        assert self.g.degree("Tehran") == 4
+        assert self.g.degree("Riyadh") == 3
+        assert self.g.degree("Baghdad") == 4
+        assert self.g.degree("Moscow") == 3
+        assert self.g.degree("Lagos") == 3
+        assert self.g.degree("Kinshasa") == 3
+        assert self.g.degree("Khartoum") == 4
+        assert self.g.degree("Johannesburg") == 3
+        assert self.g.degree("Cairo") == 5
+        assert self.g.degree("Istanbul") == 6
+        assert self.g.degree("Algiers") == 4
+        assert self.g.degree("St Petersburg") == 3
+        assert self.g.degree("Milan") == 3
+        assert self.g.degree("Essen") == 4
+        assert self.g.degree("Atlanta") == 3
+        assert self.g.degree("Chicago") == 5
+        assert self.g.degree("Montreal") == 3
+        assert self.g.degree("Washington") == 4
+        assert self.g.degree("Los Angeles") == 5
+        assert self.g.degree("San Francisco") == 4
+        assert self.g.degree("Miami") == 4
+        assert self.g.degree("Bogota") == 5
+        assert self.g.degree("Santiago") == 2
+        assert self.g.degree("Sao Paulo") == 4
+        assert self.g.degree("Buenos Aires") == 4
+        assert self.g.degree("New York") == 4
+        assert self.g.degree("London") == 4
+        assert self.g.degree("Madrid") == 5
+        assert self.g.degree("Paris") == 5
